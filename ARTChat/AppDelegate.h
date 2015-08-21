43eb7773d57479624251cb0cb4544e006fb42de6
Antonio Grimaldo <grimi94@gmail.com>
2015-08-21 16:53:34 -0500

    Initial Commit

---
+++ ARTChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARTChat
+//
+//  Created by Grimi on 8/21/15.
+//  Copyright (c) 2015 Grimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARTChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARTChat
+//
+//  Created by Grimi on 8/21/15.
+//  Copyright (c) 2015 Grimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
