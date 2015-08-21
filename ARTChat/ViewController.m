36318f9850c84b3f146cd32aac801fa2636ae8fc
Antonio Grimaldo <grimi94@gmail.com>
2015-08-21 18:00:44 -0500

    Added table view data source

--- ARTChat/ViewController.m
+++ ARTChat/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 @interface ViewController ()
+
+@property (assign, nonatomic) int                socketFD;
+@property (strong, nonatomic) dispatch_source_t socketSource;
 
 @end
 
@@ -16,12 +23,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+#pragma mark - Table data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                                            forIndexPath:indexPath];
+
+    return cell;
+}
+
+#pragma mark - Text Field Delegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+
+    return YES;
 }
 
 @end
